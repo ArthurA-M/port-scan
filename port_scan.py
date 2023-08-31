114265422913711410fd7f72324a725524e0cb0d
ArthurA-M <42844961+ArthurA-M@users.noreply.github.com>
2023-08-31 15:02:07 +0200

    Add files via upload

--- port_scan.py
+++ port_scan.py
@@ -16,38 +16,33 @@ def open_port(host, port):
     except:return False
     return True
 
-delta = 0
-
-if sys.argv[0] != "port_scan.py":
-    delta = 1
-
-if sys.argv[1 - delta] == "all": #affiche l'état de tous les ports demandés
-    for i in range(int(sys.argv[3 - delta]),int(sys.argv[4 - delta])+1):
-        if open_port(sys.argv[2 - delta], i):
-            print(f"{GREEN}[+] {sys.argv[2 - delta]}:{i} is open")
+if sys.argv[1] == "all": #affiche l'état de tous les ports demandés
+    for i in range(int(sys.argv[3]),int(sys.argv[4])+1):
+        if open_port(sys.argv[2], i):
+            print(f"{GREEN}[+] {sys.argv[2]}:{i} is open")
         else:
-            print(f"{GRAY}[-] {sys.argv[2 - delta]}:{i} is close")
+            print(f"{GRAY}[-] {sys.argv[2]}:{i} is close")
 
-elif sys.argv[1 - delta] == "open": #affiche les ports ouverts parmis ceux demandés
-    for i in range(int(sys.argv[3 - delta]),int(sys.argv[4 - delta])+1):
-        if open_port(sys.argv[2 - delta], i):
-            print(f"{GREEN}[+] {sys.argv[2 - delta]}:{i} is open")
+elif sys.argv[1] == "open": #affiche les ports ouverts parmis ceux demandés
+    for i in range(int(sys.argv[3]),int(sys.argv[4])+1):
+        if open_port(sys.argv[2], i):
+            print(f"{GREEN}[+] {sys.argv[2]}:{i} is open")
 
 elif sys.argv[1] == "listen": #affiche les changement d'états des ports
-    ports = [i for i in range(int(sys.argv[3 - delta]),int(sys.argv[4 - delta])+1) if open_port(sys.argv[2 - delta], i)]
+    ports = [i for i in range(int(sys.argv[3]),int(sys.argv[4])+1) if open_port(sys.argv[2], i)]
     while True:
-        new_ports = [i for i in range(int(sys.argv[3 - delta]),int(sys.argv[4 - delta])+1) if open_port(sys.argv[2] - delta, i)]
+        new_ports = [i for i in range(int(sys.argv[3]),int(sys.argv[4])+1) if open_port(sys.argv[2], i)]
         if ports != new_ports:
             add = [i for i in new_ports if i not in ports]
             delete = [i for i in ports if i not in new_ports]
             for i in add:
-                print(f"{GREEN}[+] {sys.argv[2 - delta]}:{i} is opened")
+                print(f"{GREEN}[+] {sys.argv[2]}:{i} is opened")
             for i in delete:
-                print(f"{GRAY}[-] {sys.argv[2 - delta]}:{i} is closed")
+                print(f"{GRAY}[-] {sys.argv[2]}:{i} is closed")
             ports = new_ports
 
 else: #affiche l'état d'un port précis
-    if open_port(sys.argv[1 - delta], sys.argv[2 - delta]):
-        print(f"{GREEN}[+] {sys.argv[1]}:{sys.argv[2 - delta]} is open")
+    if open_port(sys.argv[1], sys.argv[2]):
+        print(f"{GREEN}[+] {sys.argv[1]}:{sys.argv[2]} is open")
     else:
-        print(f"{GRAY}[-] {sys.argv[1]}:{sys.argv[2 - delta]} is close")
+        print(f"{GRAY}[-] {sys.argv[1]}:{sys.argv[2]} is close")
